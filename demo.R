96a5465b63afc74c370d212607bd60bddcedb829
Dirk Eddelbuettel <edd@debian.org>
2025-07-01 17:34:47 -0600

    Minor cleanup removing commented-out code

--- demo.R
+++ demo.R
@@ -6,7 +6,6 @@
 useRcppRedis <- function() {
 
     ensure_queue <- function(name) {
-        ##list(con = redux::hiredis(), name = name, temp = paste0(name, "_temp"))
         redis <- new(RcppRedis::Redis)
         list(con = redis,
              name = name,
@@ -17,49 +16,38 @@ useRcppRedis <- function() {
     }
 
     publish <- function(queue, message) {
-        ##invisible(queue$con$LPUSH(queue$name, message))
         invisible(queue$con$lpush(queue$todo, message))
     }
 
     list_messages <- function(queue) {
-        ##list(READY = queue$con$LRANGE(queue$name, 0, -1),
-        ##     PROCESSING = queue$con$LRANGE(queue$temp, 0, -1))
         list(RECV = queue$con$lrange(queue$todo, 0, -1),
              PROC = queue$con$lrange(queue$work, 0, -1),
              DONE = queue$con$lrange(queue$done, 0, -1))
     }
 
     try_consume <- function(queue) {
-        ##message <- queue$con$RPOPLPUSH(queue$name, queue$temp)
         message <- queue$con$lmove(queue$todo, queue$work, 'RIGHT', 'LEFT')
         if (is.null(message)) return(message)
         list(queue = queue, message = message)
     }
 
     ack <- function(message) {
-        ##invisible(message$queue$con$LREM(message$queue$temp, 1, message$message))
-        #invisible(message$queue$con$lrem(message$queue$todo, 1, message$message))
         queue <- message$queue
         msg <- queue$con$lmove(queue$work, queue$done, 'RIGHT', 'LEFT')
         stopifnot("wrong message acknowledged" = all.equal(msg, message$message))
-        #cat("'", msg, "' -- '", message, "'\n", sep="")
-        #if (is.null(message)) return(message)
-        #list(queue = queue, message = message)
         msg
     }
 
     cleanup <- function(queue) {
-        ##invisible(message$queue$con$LREM(message$queue$temp, 1, message$message))
         invisible(queue$con$del(queue$done))
-        #queue$con$lrem(queue$done, 2, "*")
     }
+    
     #############################################################################
 
     ##system("docker run -d --rm --name valkey -p 6379:6379 valkey/valkey")
     ## assume Redis running
 
     q <- ensure_queue("jobs")
-    ##q
 
     publish(q, message = "Hello world!")
     publish(q, message = "Hello again!")
@@ -87,7 +75,6 @@ useRcppRedis <- function() {
     str(try_consume(q))
 
     cleanup(q)
-    ##system("docker stop valkey")
 }
 
 
