5548ec182a82edde105a8bb54881bebe7c7c408e
Dirk Eddelbuettel <edd@debian.org>
2025-07-03 14:48:49 -0600

    Add summary message, more compact demo display

--- demo.R
+++ demo.R
@@ -29,31 +29,35 @@ print.tinyqueue <- function(x, ...) {
     cat("<tinyqueue object containing queue '", x$name, "'>\n", sep="")
 }
 
+summary.tinyqueue <- function(x, ...) {
+    .tinyqueue_validate(x)
+    #print(x)
+    cat("<tinyqueue containing todo: ", x$con$llen(x$todo), ",",
+        " work: ", x$con$llen(x$work), ",",
+        " done: ", x$con$llen(x$done),
+        ">\n", sep="")
+}
+
 useRcppRedis <- function() {
 
     ensure_queue <- function(name) {
-        ## redis <- new(RcppRedis::Redis)
-        ## structure(list(con = redis,
-        ##                name = name,
-        ##                todo = paste0(name, "_todo"),
-        ##                work = paste0(name, "_work"),
-        ##                done = paste0(name, "_done")
-        ##                ),
-        ##           class = "tinyqueue")
         tinyqueue(name)
     }
 
     publish <- function(queue, message) {
+        .tinyqueue_validate(queue)
         invisible(queue$con$lpush(queue$todo, message))
     }
 
     list_messages <- function(queue) {
+        .tinyqueue_validate(queue)
         list(RECV = queue$con$lrange(queue$todo, 0, -1),
              PROC = queue$con$lrange(queue$work, 0, -1),
              DONE = queue$con$lrange(queue$done, 0, -1))
     }
 
     try_consume <- function(queue) {
+        .tinyqueue_validate(queue)
         message <- queue$con$lmove(queue$todo, queue$work, 'RIGHT', 'LEFT')
         if (is.null(message)) return(message)
         list(queue = queue, message = message)
@@ -61,12 +65,14 @@ useRcppRedis <- function() {
 
     ack <- function(message) {
         queue <- message$queue
+        .tinyqueue_validate(queue)
         msg <- queue$con$lmove(queue$work, queue$done, 'RIGHT', 'LEFT')
         stopifnot("wrong message acknowledged" = all.equal(msg, message$message))
         msg
     }
 
     cleanup <- function(queue) {
+        .tinyqueue_validate(queue)
         invisible(queue$con$del(queue$done))
     }
     
@@ -81,27 +87,32 @@ useRcppRedis <- function() {
     publish(q, message = "Hello world!")
     publish(q, message = "Hello again!")
     cat("\n--Messages after two enqueus\n")
-    str(list_messages(q))
-
+    #str(list_messages(q))
+    summary(q)
+    
     msg <- try_consume(q)
     cat("\n--Consumed message 1\n")
-    msg$message
-    cat("\n--Messages after consume\n")
-    str(list_messages(q))
+    cat("Message is: '", msg$message, "'\n", sep="")
+    cat("--Messages after consume\n")
+    #str(list_messages(q))
+    summary(q)
 
-    print(ack(msg))
+    m <- ack(msg)
     cat("\n--Messages after ack 1\n")
-    str(list_messages(q))
+    #str(list_messages(q))
+    summary(q)
 
     msg2 <- try_consume(q)
     cat("\n--Consumed message 2\n")
-    msg2$message
-    print(ack(msg2))
-    cat("\n--Messages after ack 2\n")
-    str(list_messages(q))
+    cat("Message is: '", msg2$message, "'\n", sep="")
+    m2 <- ack(msg2)
+    cat("--Messages after ack 2\n")
+    #str(list_messages(q))
+    summary(q)
 
     cat("\n--Final try consume\n")
-    str(try_consume(q))
+    msg3 <- try_consume(q)
+    summary(q)
 
     cleanup(q)
 }
