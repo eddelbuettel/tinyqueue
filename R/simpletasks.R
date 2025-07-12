063db6e7adea90526f77c9a88f246bb1a7fe1e70
Dirk Eddelbuettel <edd@debian.org>
2025-07-12 07:18:38 -0500

    Add simple tasks class

---
+++ R/simpletasks.R
@@ -0,0 +1,64 @@
+
+taskresult <- as.factor(c("done", "failed", "skipped", "unset", "started"))
+#taskresult <- c("done", "failed", "skipped", "unset", "started")
+
+#' @title simpletask: A Simple Task Object
+#' @rdname simpletask
+#' @export simpletask
+simpletask <- function(pkg, ver, ...) {
+    UseMethod("simpletask")
+}
+
+#' @rdname simpletask
+#' @method simpletask default
+#' @export
+simpletask.default <- function(pkg, ver) {
+    structure(list(package = pkg,
+                   version = ver,
+                   result = "unset",
+                   starttime = NA_real_,
+                   endtime = NA_real_,
+                   runtime = NA_real_,
+                   runner = "",
+                   host = ""),
+              class = "simpletask")
+              
+}
+
+#' @rdname simpletask
+#' @method format simpletask 
+#' @export
+format.simpletask <- function(x, ...) {
+    txt <- paste0("<task: package '", x$package,
+                  " (", x$version, ")' in status '",
+                  x$result, "'",
+                  ##" started at ", format(x$starttime),
+                  ##" finished at ", format(x$endtime),
+                  " time ", format(x$runtime),
+                  ">")
+    txt
+}
+
+#' @rdname simpletask
+#' @method print simpletask 
+#' @export
+print.simpletask <- function(x, ...) {
+    cat(format(x, ...), "\n", sep="")
+}
+
+start_task <- function(x) {
+    ## should this consume a task ?
+    stopifnot("object 'x' must be 'simpletask' object" = inherits(x, "simpletask"))
+    x$starttime <- Sys.time()
+    x$result <- "started"
+    x
+}
+
+end_task <- function(x, res) {
+    ## should this ack a task ?
+    stopifnot("object 'x' must be 'simpletask' object" = inherits(x, "simpletask"))
+    x$endtime <- Sys.time()
+    x$runtime <- x$endtime - x$starttime
+    x$result <- res
+    x
+}    
---
+++ demo/tasks.R
@@ -0,0 +1,28 @@
+
+library(tinyqueue)
+
+q <- ensure_queue("tasks")
+summary(q)
+
+st <- simpletask("aa", "1.2")
+st
+publish(q, st)
+summary(q)
+rm(st)
+
+
+## TODO would be nice if one of these two changes status of the other
+st <- try_consume(q)  # fetches message
+st <- start_task(st)  # marks 'starting'
+print(st)
+summary(q)
+
+Sys.sleep(0.25)
+st <- end_task(st, "done")
+print(st)
+
+st <- ack(q, st, FALSE) # not checking message here
+summary(q)
+
+## cleanup
+#ignored <- q$con$del("tasks_work")
